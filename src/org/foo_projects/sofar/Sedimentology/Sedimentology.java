3a06f830fba7e42f36f2c4bfc6a4f3a61527bcb1
Auke Kok <auke-jan.h.kok@intel.com>
2013-11-28 18:58:45 -0800

    Handle underwater erosion.
    Also reindentation.

--- src/org/foo_projects/sofar/Sedimentology/Sedimentology.java
+++ src/org/foo_projects/sofar/Sedimentology/Sedimentology.java
@@ -24,27 +24,27 @@ import org.bukkit.scheduler.BukkitScheduler;
 
 
 /*
- * Sedimentology concepts 
- * 
+ * Sedimentology concepts
+ *
  * Erosion & deposition go hand in hand - at higher elevation (>64) erosion dominates, and at
  * lower elevations, sedimentation dominates.
- * 
+ *
  * Decay cycles affect rocks - stone breaks into gravel, into sand, into clay. Each step takes longer
- * 
+ *
  * Transports are wind, water, ice.
- * 
+ *
  * Wind can displace sand - create dunes.
  * Water displaces sand and clay
  * Ice displaces all blocks
- * 
+ *
  * Snow compacts into Ice
- * 
+ *
  * Blocks do not disappear, the volume of blocks remains equal througout the process
- * 
+ *
  * slope affects erosion - a higher slope means more erosion
- * 
+ *
  * deposition happens where the slope reduces - a lower slope means more deposition
- * 
+ *
  */
 
 public final class Sedimentology extends JavaPlugin {
@@ -62,9 +62,9 @@ public final class Sedimentology extends JavaPlugin {
 	private long stat_ignored_hardness;
 	private int x, y, z;
 	private long conf_attempts = 100;
-	
+
 	private Random rnd = new Random();
-	
+
 	private class offset {
 		public offset(int i, int j) {
 			x = i;
@@ -73,15 +73,15 @@ public final class Sedimentology extends JavaPlugin {
 		public int x;
 		public int z;
 	};
-	
+
 	private offset[] walker;
 	private offset[] walker2;
-	
+
 	/* our chunk map - acts as a cache map of loaded chunks and reduces
 	 * time needed to avoid map edge ssues
 	 */
 	private Map<String, Integer> ChunkMap = new HashMap<String, Integer>();
-	
+
 	public boolean ChunkMapLoaded(int x, int z) {
 		String key = "(" + x + "," + z + ")";
 		if (ChunkMap.get(key) == null)
@@ -98,7 +98,7 @@ public final class Sedimentology extends JavaPlugin {
 		String key = "(" + x + "," + z + ")";
 		ChunkMap.put(key, 0);
 	}
-	
+
 	/* helper function to find lowest deposit elevation ignoring water */
 	private int findDepositLocation(World world, int x, int y, int z) {
 		int yy = y;
@@ -119,19 +119,22 @@ public final class Sedimentology extends JavaPlugin {
 
 	public void sed(World world) {
 		stat_considered++;
-       	Chunk ChunkList[] = world.getLoadedChunks();
-    	Chunk c = ChunkList[(int) Math.abs(rnd.nextDouble() * ChunkList.length)];
-    	
-    	double hardness;
-    	double resistance;
+	   	Chunk ChunkList[] = world.getLoadedChunks();
+		Chunk c = ChunkList[(int) Math.abs(rnd.nextDouble() * ChunkList.length)];
+
+		double hardness;
+		double resistance;
 		double waterfactor;
 		double vegetationfactor;
 		double stormfactor;
-		
+
+		boolean underwater = false;
+		boolean undermovingwater = false;
+
 		int bx = (int)(Math.round(rnd.nextDouble() * 16));
 		int bz = (int)(Math.round(rnd.nextDouble() * 16));
 
-    	/* don't bother touching chunks at the edges */
+		/* don't bother touching chunks at the edges */
 		for (int xx = c.getX() - 1 ; xx <= c.getX() + 1; xx++) {
 			for (int zz = c.getZ() - 1 ; zz <= c.getZ() + 1; zz++) {
 				if (ChunkMapLoaded(xx, zz) == false) {
@@ -140,84 +143,104 @@ public final class Sedimentology extends JavaPlugin {
 				}
 			}
 		}
-		
-    	x = bx + (c.getX() * 16);
-    	z = bz + (c.getZ() * 16);
 
-    	y = world.getHighestBlockYAt(x, z) - 1;
-    	Block b = world.getBlockAt(x, y, z);
-    	
-    	/* filter out blocks we don't erode right now */
-    	hardness = 1.0;
-    	resistance = 1.0;
-    	switch (b.getType()) {
-    		case SOIL:
-    		case DIRT:
-    		case GRASS:
-    		case SAND:
-    			break;
-    		case GRAVEL:
-    			hardness = 0.15;
-    			resistance = 0.75;
-    			break;
-    		case CLAY:
-    			resistance = 0.3;
-    			break;
-    		case SANDSTONE:
-    			hardness = 0.05;
-    			resistance = 0.05;
-    			break;
-    		case COBBLESTONE:
-    			hardness = 0.05;
-    			resistance = 0.05;
-    			break;
-    		case STONE:
-    			hardness = 0.01;
-    			resistance = 0.01;
-    			break;
-    		default:
-    			stat_ignored_type++;
-    			return;
-    	}
+		x = bx + (c.getX() * 16);
+		z = bz + (c.getZ() * 16);
 
-    	/* lower overall chance due to lack of water */
-    	stormfactor = 0.1;
-    	if (world.hasStorm())
-    		stormfactor = 1.0;
-    	
-    	if (rnd.nextDouble() > stormfactor) {
-    		stat_ignored_storm++;
-    		return;
-    	}
+		/* find highest block, even if underwater */
+		y = world.getHighestBlockYAt(x, z) - 1;
+		switch (world.getBlockAt(x, y, z).getType()) {
+			case WATER:
+				undermovingwater = true;
+			case STATIONARY_WATER:
+				underwater = true;
+				y = findDepositLocation(world, x, y, z) - 1;
+				break;
+			//FIXME: start handling ice/snow from here
+			default:
+				break;
+		}
+
+		Block b = world.getBlockAt(x, y, z);
+
+		/* filter out blocks we don't erode right now */
+		hardness = 1.0;
+		resistance = 1.0;
+		switch (b.getType()) {
+			case SOIL:
+			case DIRT:
+			case GRASS:
+			case SAND:
+				break;
+			case GRAVEL:
+				hardness = 0.15;
+				resistance = 0.75;
+				break;
+			case CLAY:
+				resistance = 0.3;
+				break;
+			case SANDSTONE:
+				hardness = 0.05;
+				resistance = 0.05;
+				break;
+			case COBBLESTONE:
+				hardness = 0.05;
+				resistance = 0.05;
+				break;
+			case STONE:
+				hardness = 0.01;
+				resistance = 0.01;
+				break;
+			default:
+				stat_ignored_type++;
+				return;
+		}
+
+		/* lower overall chance due to lack of water */
+		stormfactor = 0.1;
+		if (world.hasStorm())
+			stormfactor = 1.0;
+
+		if ((!underwater) && (rnd.nextDouble() > stormfactor)) {
+			stat_ignored_storm++;
+			return;
+		}
 
 		// water increases displacement/degradation
-    	waterfactor = 0.1;
+		waterfactor = 0.01; //this is probably too low
+
+		if (undermovingwater)
+			waterfactor = 1.0;
+		else if (underwater)
+			waterfactor = 0.5;
+		else {
 waterloop:
-    	for (int xx = x - 2; xx < x + 2; xx++) {
-    		for (int zz = z - 2; zz < z + 2; zz++) {
-    			for (int yy = y - 2; yy < y + 2; yy++) {
-    				switch(world.getBlockAt(xx, yy, zz).getType()) {
-    					case WATER:
-    						waterfactor = 1.0;
-    						break waterloop;
-    					case STATIONARY_WATER:
-    						waterfactor = 0.5;
-    						break;
-    					default:
-    						break;
-    				}
-    			}
-    		}
-    	}
-    	
+			for (int xx = x - 2; xx < x + 2; xx++) {
+				for (int zz = z - 2; zz < z + 2; zz++) {
+					for (int yy = y - 2; yy < y + 2; yy++) {
+						switch(world.getBlockAt(xx, yy, zz).getType()) {
+						case WATER:
+							waterfactor = 0.25;
+							break waterloop;
+						case STATIONARY_WATER:
+							waterfactor = 0.125;
+							break;
+						default:
+							break;
+						}
+					}
+				}
+			}
+		}
+
 		if (rnd.nextDouble() > waterfactor) {
 			stat_ignored_water++;
 			return;
 		}
 
-    	// vegetation slows down displacement
-    	vegetationfactor = 1.0;
-vegetationloop:
+		// vegetation slows down displacement
+		vegetationfactor = 1.0;
+
 		for (int xx = x - 3; xx < x + 3; xx++) {
 			for (int zz = z - 3; zz < z + 3; zz++) {
 				for (int yy = y - 3; yy < y + 3; yy++) {
@@ -238,8 +261,13 @@ vegetationloop:
 						case MELON_BLOCK:
 						case PUMPKIN:
 						case VINE:
-							vegetationfactor = 0.1;
-							break vegetationloop;
+						case SUGAR_CANE:
+							/* distance to vegetation: 0.3 to 3.0 */
+							double d = (Math.abs(xx - x) + Math.abs(yy - y) + Math.abs(zz -z)) / 3.0;
+							double f = 0.5 / (4.0 - d); /* 0.5 / (3.7 -> 1.0) */
+							if (f < vegetationfactor)
+								vegetationfactor = f;
+							break; //vegetationloop;
 						default:
 							break;
 					}
@@ -251,150 +279,165 @@ vegetationloop:
 			stat_ignored_vegetation++;
 			return;
 		}
-		
-    	/* displace block? */
+
+		/* displace block? */
 displace:
-    	if (true) {
-    		// displace should factor in elevation
-	
-    		
-	    	// find the most suitable target location to move this block to
-    		int start = (int)(rnd.nextDouble() * 8.0);
-    		int lowest = y - 1;
-    		int lowestIndex = -1;
-    		int i = 0;
-    		int tx = 0, ty = 0, tz = 0;
-    		
-    		while (true) {
-    			offset o = walker[(start+i) % 8];
-    			int h = findDepositLocation(world, x + o.x, lowest, z + o.z);
-    			if (h < lowest) {
-    				lowest = h;
-    				lowestIndex = (start+i) % 8;
-    			}
-    				
-    			/* break out, we've looked all around */
-    			if (i++ == 7)
-    				break;
-    		}
- 
-    		if (lowestIndex != -1) {
-    			tx = x + walker[lowestIndex].x;
-    			ty = lowest;
-    			tz = z + walker[lowestIndex].z;
-    		}
-	    	
-    		// sand can be displaced further
-	    	if ((lowestIndex == -1) && (b.getType() == Material.SAND)) { 
-	    		start = (int)(rnd.nextDouble() * 16.0);
-	    		lowest = y - 1;
-	    		lowestIndex = -1;
-	    		i = 0;
-	    		while (true) {
-	    			offset o = walker2[(start+i) % 16];
-	    			int h = findDepositLocation(world, x + o.x, lowest, z + o.z);
-	    			if (h < lowest) {
-	    				lowest = h;
-	    				lowestIndex = (start+i) % 16;
-	    			}
-	    				
-	    			/* break out, we've looked all around */
-	    			if (i++ == 15)
-	    				break;
-	    		}
-	    		if (lowestIndex != -1) {
-	    			tx = x + walker2[lowestIndex].x;
-	    			ty = lowest;
-	    			tz = z + walker2[lowestIndex].z;
-	    		}
-	    	}
+		if (true) {
+			// displace should factor in elevation
 
-	    	/* flat? */
-    		if (lowestIndex == -1) {
-    			break displace;
-    		}
 
-    		/* roll to move it */
-    		if (rnd.nextDouble() > resistance) {
-    			stat_ignored_resistance++;
-    			return;
-    		}
-	
-    		/* It's time to move it, move it. */
-    		Block t = world.getBlockAt(tx, ty, tz);
-    		switch (t.getType()) {
-    			case AIR:
-    			case WATER:
-    			case STATIONARY_WATER:
-    				t.setType(b.getType());
-    				b.setType(Material.AIR);
-    				stat_displaced++;
-    				return;
-    			default:
-    				/* figure out how this happened */
-    				stat_errors++;
-    				getLogger().info("Attempted to move into a block of " + t.getType().name());
-    				return;
-    		}
-    	}
+			// find the most suitable target location to move this block to
+			int start = (int)(rnd.nextDouble() * 8.0);
+			int lowest = y - 1;
+			int lowestIndex = -1;
+			int i = 0;
+			int tx = 0, ty = 0, tz = 0;
 
-    	/* degrade block? */
-    	
-    	// degrade should factor in elevation?
-    	
-    	// should detect the presence of water or ice near and factor in.
-    	
-    	/* do not decay sand further unless in a wet Biome */
-    	if (b.getType() == Material.SAND) {
-	    	switch (b.getBiome()) {
-	    		case RIVER:
-	    		case OCEAN:
-	    		case SWAMPLAND:
-	    		case FROZEN_RIVER:
-	    		case FROZEN_OCEAN:
-	    			break;
-	    		default:
-	    			stat_ignored_sand++;
-	    			return;
-	    	}
-    	}
+			while (true) {
+				offset o = walker[(start+i) % 8];
+				int h = findDepositLocation(world, x + o.x, lowest, z + o.z);
+				if (h < lowest) {
+					lowest = h;
+					lowestIndex = (start+i) % 8;
+				}
 
-    	if (rnd.nextDouble() < hardness) {
-    		switch (b.getType()) {
-    			case DIRT:
-    				b.setType(Material.SAND);
-    				break;
-    			case SOIL:
-    			case GRASS:
-    				b.setType(Material.DIRT);
-    				break;
-    			case SAND:
-    				b.setType(Material.CLAY);
-    				break;
-    			case GRAVEL:
-    				b.setType(Material.DIRT);
-    				break;
-    			case CLAY:
-    				/* we can displace clay, but not degrade */
-       				return;
-    			case SANDSTONE:
-    			case COBBLESTONE:
-    				b.setType(Material.GRAVEL);
-    				break;
-    			case STONE:
-    				b.setType(Material.COBBLESTONE);
-    				break;
-    			default:
-    				stat_errors++;
-    				return;
-    		}
-    	} else {
-    		stat_ignored_hardness++;
-    		return;
-    	}
+				/* break out, we've looked all around */
+				if (i++ == 7)
+					break;
+			}
 
-    	stat_degraded++;
-    };
+			if (lowestIndex != -1) {
+				tx = x + walker[lowestIndex].x;
+				ty = lowest;
+				tz = z + walker[lowestIndex].z;
+			}
+
+			// sand can be displaced further
+			if ((lowestIndex == -1) && (b.getType() == Material.SAND)) {
+				start = (int)(rnd.nextDouble() * 16.0);
+				lowest = y - 1;
+				lowestIndex = -1;
+				i = 0;
+				while (true) {
+					offset o = walker2[(start+i) % 16];
+					int h = findDepositLocation(world, x + o.x, lowest, z + o.z);
+					if (h < lowest) {
+						lowest = h;
+						lowestIndex = (start+i) % 16;
+					}
+
+					/* break out, we've looked all around */
+					if (i++ == 15)
+						break;
+				}
+				if (lowestIndex != -1) {
+					tx = x + walker2[lowestIndex].x;
+					ty = lowest;
+					tz = z + walker2[lowestIndex].z;
+				}
+			}
+
+			/* flat? */
+			if (lowestIndex == -1) {
+				break displace;
+			}
+
+			/* roll to move it */
+			if (rnd.nextDouble() > resistance) {
+				stat_ignored_resistance++;
+				return;
+			}
+
+			/* It's time to move it, move it. */
+			Block t = world.getBlockAt(tx, ty, tz);
+			switch (t.getType()) {
+				case AIR:
+				case WATER:
+				case STATIONARY_WATER:
+					t.setType(b.getType());
+					b.setType(Material.AIR);
+					stat_displaced++;
+					return;
+				default:
+					/* figure out how this happened */
+					stat_errors++;
+					getLogger().info("Attempted to move into a block of " + t.getType().name());
+					return;
+			}
+		}
+
+		/* degrade block? */
+
+		// degrade should factor in elevation?
+
+		// should detect the presence of water or ice near and factor in.
+
+		/* do not decay sand further unless in a wet Biome */
+		if (b.getType() == Material.SAND) {
+			switch (b.getBiome()) {
+				case RIVER:
+				case OCEAN:
+				case SWAMPLAND:
+				case FROZEN_RIVER:
+				case FROZEN_OCEAN:
+					break;
+				default:
+					stat_ignored_sand++;
+					return;
+			}
+		}
+
+		/* For now, don't decay into sand for biomes that are mostly dirtish */
+		if (b.getType() == Material.DIRT){
+			switch (b.getBiome()) {
+				case BEACH:
+				case DESERT:
+				case DESERT_HILLS:
+				case FROZEN_OCEAN:
+				case FROZEN_RIVER:
+					break;
+				default:
+					stat_ignored_sand++;
+					return;
+			}
+		}
+
+		if (rnd.nextDouble() < hardness) {
+			switch (b.getType()) {
+				case DIRT:
+					b.setType(Material.SAND);
+					break;
+				case SOIL:
+				case GRASS:
+					b.setType(Material.DIRT);
+					break;
+				case SAND:
+					b.setType(Material.CLAY);
+					break;
+				case GRAVEL:
+					b.setType(Material.DIRT);
+					break;
+				case CLAY:
+					/* we can displace clay, but not degrade */
+	   				return;
+				case SANDSTONE:
+				case COBBLESTONE:
+					b.setType(Material.GRAVEL);
+					break;
+				case STONE:
+					b.setType(Material.COBBLESTONE);
+					break;
+				default:
+					stat_errors++;
+					return;
+			}
+		} else {
+			stat_ignored_hardness++;
+			return;
+		}
+
+		stat_degraded++;
+	};
 
 	private class SedimentologyRunnable implements Runnable {
 		public void run() {
@@ -409,9 +452,9 @@ displace:
 				conf_attempts = Long.parseLong(split[0]);
 				return true;
 			}
-			
+
 			World world = org.bukkit.Bukkit.getWorld("world");
-	    	Chunk ChunkList[] = world.getLoadedChunks();
+			Chunk ChunkList[] = world.getLoadedChunks();
 			String msg = String.format("Sedimentology: \n" +
 						"considered %d, displaced %d, degraded %d blocks in %d chunks %d errors\nlast one at %d %d %d\n" +
 						"ignored: edge %d, type %d, storm %d, vegetation %d, resistance %d, water %d, sand %d, hardness %d",
@@ -419,7 +462,7 @@ displace:
 						x, y, z,
 						stat_ignored_edge, stat_ignored_type, stat_ignored_storm, stat_ignored_vegetation,
 						stat_ignored_resistance, stat_ignored_water, stat_ignored_sand, stat_ignored_hardness);
-			
+
 			if (!(sender instanceof Player)) {
 				getLogger().info(msg);
 			} else {
@@ -429,7 +472,7 @@ displace:
 			return true;
 		}
 	}
-	
+
 	class SedimentologyListener implements Listener {
 		@EventHandler
 		public void onChunkLoadEvent(ChunkLoadEvent event) {
@@ -438,7 +481,7 @@ displace:
 			int z = c.getZ();
 			ChunkMapLoad(x, z);
 		}
-		
+
 		@EventHandler
 		public void onChunkUnloadEvent(ChunkUnloadEvent event) {
 			Chunk c = event.getChunk();
@@ -447,55 +490,55 @@ displace:
 			ChunkMapUnload(x, z);
 		}
 	}
-	
+
 	public void onEnable() {
-        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
-        
-        getCommand("sedimentology").setExecutor(new SedimentologyCommand());
-        
-        /* fill initial chunkmap here */
-        World world = org.bukkit.Bukkit.getWorld("world");
-    	Chunk ChunkList[] = world.getLoadedChunks();
-    	for (Chunk c: ChunkList) {
-    		int x = c.getX();
+		BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+
+		getCommand("sedimentology").setExecutor(new SedimentologyCommand());
+
+		/* fill initial chunkmap here */
+		World world = org.bukkit.Bukkit.getWorld("world");
+		Chunk ChunkList[] = world.getLoadedChunks();
+		for (Chunk c: ChunkList) {
+			int x = c.getX();
 			int z = c.getZ();
 			ChunkMapLoad(x, z);
-    	}
-        
-    	/* even handler takes care of updating it from there */
-        getServer().getPluginManager().registerEvents(new SedimentologyListener(), this);
+		}
 
-        /* these are used to displace blocks */
-        walker = new offset[8];
-        walker[0] = new offset(-1,-1);
-        walker[1] = new offset(0,-1);
-        walker[2] = new offset(1,-1);
-        walker[3] = new offset(1,0);
-        walker[4] = new offset(1,1);
-        walker[5] = new offset(0,1);
-        walker[6] = new offset(-1,1);
-        walker[7] = new offset(-1,0);
-        
-        walker2 = new offset[16];
-        walker2[0] = new offset(-2,-2);
-        walker2[1] = new offset(-1,-2);
-        walker2[2] = new offset(0,-2);
-        walker2[3] = new offset(1,-2);
-        walker2[4] = new offset(2,-2);
-        walker2[5] = new offset(2,-1);
-        walker2[6] = new offset(2,0);
-        walker2[7] = new offset(2,1);
-        walker2[8] = new offset(2,2);
-        walker2[9] = new offset(1,2);
-        walker2[10] = new offset(0,2);
-        walker2[11] = new offset(-1,2);
-        walker2[12] = new offset(-2,2);
-        walker2[13] = new offset(-2,1);
-        walker2[14] = new offset(-2,0);
-        walker2[15] = new offset(-2,-1);
-        
-        scheduler.scheduleSyncRepeatingTask(this, new SedimentologyRunnable(), 1L, 1L);
-    }
+		/* even handler takes care of updating it from there */
+		getServer().getPluginManager().registerEvents(new SedimentologyListener(), this);
+
+		/* these are used to displace blocks */
+		walker = new offset[8];
+		walker[0] = new offset(-1,-1);
+		walker[1] = new offset(0,-1);
+		walker[2] = new offset(1,-1);
+		walker[3] = new offset(1,0);
+		walker[4] = new offset(1,1);
+		walker[5] = new offset(0,1);
+		walker[6] = new offset(-1,1);
+		walker[7] = new offset(-1,0);
+
+		walker2 = new offset[16];
+		walker2[0] = new offset(-2,-2);
+		walker2[1] = new offset(-1,-2);
+		walker2[2] = new offset(0,-2);
+		walker2[3] = new offset(1,-2);
+		walker2[4] = new offset(2,-2);
+		walker2[5] = new offset(2,-1);
+		walker2[6] = new offset(2,0);
+		walker2[7] = new offset(2,1);
+		walker2[8] = new offset(2,2);
+		walker2[9] = new offset(1,2);
+		walker2[10] = new offset(0,2);
+		walker2[11] = new offset(-1,2);
+		walker2[12] = new offset(-2,2);
+		walker2[13] = new offset(-2,1);
+		walker2[14] = new offset(-2,0);
+		walker2[15] = new offset(-2,-1);
+
+		scheduler.scheduleSyncRepeatingTask(this, new SedimentologyRunnable(), 1L, 1L);
+	}
 }
 
 
