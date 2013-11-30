57e5b0019e8a830f4684b99fd62ca3acf4ea5251
Auke Kok <auke-jan.h.kok@intel.com>
2013-11-29 22:24:06 -0800

    most of v2 in this commit:
    - sounds played when a block is dropped.
    - walker algorithm changed, will drop blocks closer before
      going diagonal, but still prefer lowest deposit location.
    - command overhauled: list, enable and disable worlds.
    - falling blocks will now "crush" most plants, but not logs.
    - decay to sand even in non-desertlike biomes as long as it
      is underwater, same for clay in non-oceanlike biomes.
    - config file now stores all params.
    - shorthand '/sed' also gives access to all commands.

--- src/org/foo_projects/sofar/Sedimentology/Sedimentology.java
+++ src/org/foo_projects/sofar/Sedimentology/Sedimentology.java
@@ -1,6 +1,10 @@
 package org.foo_projects.sofar.Sedimentology;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 import java.util.Map;
 
@@ -17,11 +21,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.World;
+import org.bukkit.World.Environment;
 
 import org.bukkit.scheduler.BukkitScheduler;
-
 
 /*
  * Sedimentology concepts
@@ -61,7 +67,16 @@ public final class Sedimentology extends JavaPlugin {
 	private long stat_ignored_sand;
 	private long stat_ignored_hardness;
 	private int x, y, z;
-	private long conf_attempts = 100;
+	private long conf_blocks = 10;
+
+	public static final List<String> defaultWorldList = Collections.unmodifiableList(Arrays.asList("world"));
+
+	private class sedWorld {
+		World world;
+		Map<String, Integer> chunkMap;
+	}
+
+	private List<sedWorld> sedWorldList;
 
 	private Random rnd = new Random();
 
@@ -74,39 +89,48 @@ public final class Sedimentology extends JavaPlugin {
 		public int z;
 	};
 
-	private offset[] walker;
-	private offset[] walker2;
-
-	/* our chunk map - acts as a cache map of loaded chunks and reduces
-	 * time needed to avoid map edge ssues
-	 */
-	private Map<String, Integer> ChunkMap = new HashMap<String, Integer>();
-
-	public boolean ChunkMapLoaded(int x, int z) {
+	public boolean ChunkMapLoaded(Map<String, Integer> chunkMap, int x, int z) {
 		String key = "(" + x + "," + z + ")";
-		if (ChunkMap.get(key) == null)
+		if (chunkMap.get(key) == null)
 			return false;
-		return ChunkMap.get(key) == 1;
+		return chunkMap.get(key) == 1;
 	}
 
-	public void ChunkMapLoad(int x, int z) {
+	public void ChunkMapLoad(Map<String, Integer> chunkMap, int x, int z) {
 		String key = "(" + x + "," + z + ")";
-		ChunkMap.put(key, 1);
+		chunkMap.put(key, 1);
 	}
 
-	public void ChunkMapUnload(int x, int z) {
+	public void ChunkMapUnload(Map<String, Integer> chunkMap, int x, int z) {
 		String key = "(" + x + "," + z + ")";
-		ChunkMap.put(key, 0);
+		chunkMap.put(key, 0);
 	}
 
 	/* helper function to find lowest deposit elevation ignoring water */
 	private int findDepositLocation(World world, int x, int y, int z) {
 		int yy = y;
 		while (true) {
+			//FIXME - this needs to "crush" small vegetation - right now it puts blocks on top of flowers...
 			switch (world.getBlockAt(x, yy, z).getType()) {
 				case AIR:
 				case WATER:
 				case STATIONARY_WATER:
+				case LEAVES:
+				case CACTUS:
+				case SAPLING:
+				case LONG_GRASS:
+				case DEAD_BUSH:
+				case YELLOW_FLOWER:
+				case RED_ROSE:
+				case BROWN_MUSHROOM:
+				case RED_MUSHROOM:
+				case CROPS:
+				case MELON_STEM:
+				case PUMPKIN_STEM:
+				case MELON_BLOCK:
+				case PUMPKIN:
+				case VINE:
+				case SUGAR_CANE:
 					yy--;
 					if (yy == 0)
 						return yy;
@@ -117,8 +141,9 @@ public final class Sedimentology extends JavaPlugin {
 		}
 	}
 
-	public void sed(World world) {
+	public void sed(sedWorld sedworld) {
 		stat_considered++;
+		World world = sedworld.world;
 		Chunk ChunkList[] = world.getLoadedChunks();
 		Chunk c = ChunkList[(int) Math.abs(rnd.nextDouble() * ChunkList.length)];
 
@@ -137,7 +162,7 @@ public final class Sedimentology extends JavaPlugin {
 		/* don't bother touching chunks at the edges */
 		for (int xx = c.getX() - 1 ; xx <= c.getX() + 1; xx++) {
 			for (int zz = c.getZ() - 1 ; zz <= c.getZ() + 1; zz++) {
-				if (ChunkMapLoaded(xx, zz) == false) {
+				if (ChunkMapLoaded(sedworld.chunkMap, xx, zz) == false) {
 					stat_ignored_edge++;
 					return;
 				}
@@ -170,7 +195,13 @@ public final class Sedimentology extends JavaPlugin {
 			case SOIL:
 			case DIRT:
 			case GRASS:
+				break;
 			case SAND:
+				/*
+				 * breaking sand into clay is hard, this also prevents all
+				 * sand from turning into clay
+				 */
+				hardness = 0.01;
 				break;
 			case GRAVEL:
 				hardness = 0.15;
@@ -287,64 +318,40 @@ waterloop:
 		/* displace block? */
 displace:
 		if (true) {
-			// displace should factor in elevation
+			//FIXME displace should factor in elevation, but it doesn't git well
+			// in our implementation. Perhaps we can modify hardness and increase it
+			// for high elevations
 
+			int step, steps;
 
-			// find the most suitable target location to move this block to
-			int start = (int)(rnd.nextDouble() * 8.0);
+			/* find the most suitable target location to move this block to */
+			if (b.getType() == Material.SAND)
+				steps = 24;
+			else
+				steps = 8;
+
 			int lowest = y - 1;
-			int lowestIndex = -1;
-			int i = 0;
+			offset lowestoffset = new offset(0, 0);
+			offset o = new offset(0, 0);
 			int tx = 0, ty = 0, tz = 0;
 
-			while (true) {
-				offset o = walker[(start+i) % 8];
+			for (step = 0; step < steps; step++) {
+				o = walker_f(o);
 				int h = findDepositLocation(world, x + o.x, lowest, z + o.z);
+
 				if (h < lowest) {
 					lowest = h;
-					lowestIndex = (start+i) % 8;
-				}
-
-				/* break out, we've looked all around */
-				if (i++ == 7)
-					break;
-			}
-
-			if (lowestIndex != -1) {
-				tx = x + walker[lowestIndex].x;
-				ty = lowest;
-				tz = z + walker[lowestIndex].z;
-			}
-
-			// sand can be displaced further
-			if ((lowestIndex == -1) && (b.getType() == Material.SAND)) {
-				start = (int)(rnd.nextDouble() * 16.0);
-				lowest = y - 1;
-				lowestIndex = -1;
-				i = 0;
-				while (true) {
-					offset o = walker2[(start+i) % 16];
-					int h = findDepositLocation(world, x + o.x, lowest, z + o.z);
-					if (h < lowest) {
-						lowest = h;
-						lowestIndex = (start+i) % 16;
-					}
-
-					/* break out, we've looked all around */
-					if (i++ == 15)
-						break;
-				}
-				if (lowestIndex != -1) {
-					tx = x + walker2[lowestIndex].x;
-					ty = lowest;
-					tz = z + walker2[lowestIndex].z;
+					lowestoffset = o;
 				}
 			}
 
-			/* flat? */
-			if (lowestIndex == -1) {
+			/* flat ? */
+			if (lowest == y - 1)
 				break displace;
-			}
+
+			tx = x + lowestoffset.x;
+			ty = lowest;
+			tz = z + lowestoffset.z;
 
 			/* roll to move it */
 			if (rnd.nextDouble() > resistance) {
@@ -358,8 +365,54 @@ displace:
 				case AIR:
 				case WATER:
 				case STATIONARY_WATER:
+				case LEAVES:
+				case CACTUS:
+				case SAPLING:
+				case LONG_GRASS:
+				case DEAD_BUSH:
+				case YELLOW_FLOWER:
+				case RED_ROSE:
+				case BROWN_MUSHROOM:
+				case RED_MUSHROOM:
+				case CROPS:
+				case MELON_STEM:
+				case PUMPKIN_STEM:
+				case MELON_BLOCK:
+				case PUMPKIN:
+				case VINE:
+				case SUGAR_CANE:
+					Sound snd;
+
 					t.setType(b.getType());
 					b.setType(Material.AIR);
+
+					/* play a sound at the deposition area */
+					//FIXME: splash sound when dropping into water
+					if (y - ty > 2) {
+						snd = Sound.FALL_BIG;
+					} else {
+						switch(b.getType()) {
+						case CLAY:
+						case SAND:
+							snd = Sound.DIG_SAND;
+						case DIRT:
+						case GRASS:
+							snd = Sound.DIG_GRASS;
+							break;
+						case GRAVEL:
+							snd = Sound.DIG_GRAVEL;
+							break;
+						case COBBLESTONE:
+						case STONE:
+							snd = Sound.DIG_STONE;
+							break;
+						default:
+							snd = Sound.FALL_SMALL;
+							break;
+						}
+					}
+					world.playSound(new Location(world, tx, ty, tz), snd, 1, 1);
+
 					stat_displaced++;
 					return;
 				default:
@@ -374,9 +427,9 @@ displace:
 
 		// degrade should factor in elevation?
 
-		// should detect the presence of water or ice near and factor in.
+		//FIXME should detect the presence of ice near and factor in.
 
-		/* do not decay sand further unless in a wet Biome */
+		/* do not decay sand further unless in a wet Biome, and under water */
 		if (b.getType() == Material.SAND) {
 			switch (b.getBiome()) {
 				case RIVER:
@@ -384,21 +437,24 @@ displace:
 				case SWAMPLAND:
 				case FROZEN_RIVER:
 				case FROZEN_OCEAN:
-					break;
+					if (underwater)
+						break;
 				default:
 					stat_ignored_sand++;
 					return;
 			}
 		}
 
-		/* For now, don't decay into sand for biomes that are mostly dirtish */
+		/* For now, don't decay into sand for biomes that are mostly dirtish, unless under water */
 		if (b.getType() == Material.DIRT){
 			switch (b.getBiome()) {
+				case OCEAN:
 				case BEACH:
 				case DESERT:
 				case DESERT_HILLS:
 				case FROZEN_OCEAN:
 				case FROZEN_RIVER:
+					if (underwater)
 					break;
 				default:
 					stat_ignored_sand++;
@@ -423,7 +479,7 @@ displace:
 					break;
 				case CLAY:
 					/* we can displace clay, but not degrade */
-	   				return;
+					return;
 				case SANDSTONE:
 				case COBBLESTONE:
 					b.setType(Material.GRAVEL);
@@ -445,27 +501,80 @@ displace:
 
 	private class SedimentologyRunnable implements Runnable {
 		public void run() {
-			for (int i = 0; i < conf_attempts; i++)
-				sed(org.bukkit.Bukkit.getWorld("world"));
-		};
+			for (sedWorld sedWorldItem: sedWorldList) {
+				for (int j = 0; j < conf_blocks; j++)
+					sed(sedWorldItem);
+			}
+		}
 	}
 
 	class SedimentologyCommand implements CommandExecutor {
 		public boolean onCommand(CommandSender sender, Command command, String label, String[] split) {
-			if (split.length == 1) {
-				conf_attempts = Long.parseLong(split[0]);
-				return true;
-			}
+			String msg = "unknown command";
+			String helpmsg = "\n" + 
+					"/sedimentology help - display this help message\n" +
+					"/sedimentology stats - display statistics\n" +
+					"/sedimentology list - display enabled worlds\n" +
+					"/sedimentology blocks <int> - sed number of block attempts per cycle\n" +
+					"/sedimentology enable <world> - enable for world\n" +
+					"/sedimentology disable <world> - enable for world";
 
-			World world = org.bukkit.Bukkit.getWorld("world");
-			Chunk ChunkList[] = world.getLoadedChunks();
-			String msg = String.format("Sedimentology: \n" +
-						"considered %d, displaced %d, degraded %d blocks in %d chunks %d errors\nlast one at %d %d %d\n" +
-						"ignored: edge %d, type %d, storm %d, vegetation %d, resistance %d, water %d, sand %d, hardness %d",
-						stat_considered, stat_displaced, stat_degraded, ChunkList.length, stat_errors,
-						x, y, z,
-						stat_ignored_edge, stat_ignored_type, stat_ignored_storm, stat_ignored_vegetation,
-						stat_ignored_resistance, stat_ignored_water, stat_ignored_sand, stat_ignored_hardness);
+			if (split.length >= 1) {
+				switch (split[0]) {
+					case "blocks":
+						if (split.length == 2) {
+							conf_blocks = Long.parseLong(split[1]);
+							getConfig().set("blocks",  conf_blocks);
+							saveConfig();
+						}
+						msg = "number of blocks set to " + conf_blocks;
+						break;
+					case "list":
+						msg = "plugin enabled for worlds:\n";
+						for (sedWorld s: sedWorldList) {
+							msg += "- " + s.world.getName() + "\n";
+						}
+						break;
+					case "enable":
+						if (split.length != 2) {
+							msg = "enable requires a world name";
+							break;
+						};
+						if (!enableWorld(split[1]))
+							msg = "unable to enable for world \"" + split[1] + "\"";
+						else
+							msg = "enabled for world \"" + split[1] + "\"";
+						break;
+					case "disable":
+						if (split.length != 2) {
+							msg = "disable requires a world name";
+							break;
+						};
+						if (!disableWorld(split[1]))
+							msg = "unable to disable for world \"" + split[1] + "\"";
+						else
+							msg = "disabled for world \"" + split[1] + "\"";
+						break;
+					case "stats":
+						World world = org.bukkit.Bukkit.getWorld("world");
+						Chunk ChunkList[] = world.getLoadedChunks();
+						msg = String.format("blocks per cycle: %d\n" +
+									"considered %d, displaced %d, degraded %d blocks in %d chunks %d errors\nlast one at %d %d %d\n" +
+									"ignored: edge %d, type %d, storm %d, vegetation %d, resistance %d, water %d, sand %d, hardness %d",
+									conf_blocks,
+									stat_considered, stat_displaced, stat_degraded, ChunkList.length, stat_errors,
+									x, y, z,
+									stat_ignored_edge, stat_ignored_type, stat_ignored_storm, stat_ignored_vegetation,
+									stat_ignored_resistance, stat_ignored_water, stat_ignored_sand, stat_ignored_hardness);
+						break;
+					case "help":
+					default:
+						msg = helpmsg;
+						break;
+				}
+			} else {
+				msg = helpmsg;
+			}
 
 			if (!(sender instanceof Player)) {
 				getLogger().info(msg);
@@ -480,71 +589,197 @@ displace:
 	class SedimentologyListener implements Listener {
 		@EventHandler
 		public void onChunkLoadEvent(ChunkLoadEvent event) {
-			Chunk c = event.getChunk();
-			int x = c.getX();
-			int z = c.getZ();
-			ChunkMapLoad(x, z);
+			World w = event.getWorld();
+			for (sedWorld ww: sedWorldList) {
+				if (ww.world.equals(w)) {
+					Chunk c = event.getChunk();
+					int x = c.getX();
+					int z = c.getZ();
+					ChunkMapLoad(ww.chunkMap, x, z);
+				}
+			}
 		}
 
 		@EventHandler
 		public void onChunkUnloadEvent(ChunkUnloadEvent event) {
-			Chunk c = event.getChunk();
-			int x = c.getX();
-			int z = c.getZ();
-			ChunkMapUnload(x, z);
+			World w = event.getWorld();
+			for (sedWorld ww: sedWorldList) {
+				if (ww.world.equals(w)) {
+					Chunk c = event.getChunk();
+					int x = c.getX();
+					int z = c.getZ();
+					ChunkMapUnload(ww.chunkMap, x, z);
+				}
+			}
 		}
 	}
 
-	public void onEnable() {
-		BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+	public boolean enableWorld(String worldName)
+	{
+		getLogger().info("enabling world \"" + worldName + "\"");
 
-		getCommand("sedimentology").setExecutor(new SedimentologyCommand());
+		if (org.bukkit.Bukkit.getWorld(worldName) == null) {
+			getLogger().info("No such world");
+			return false;
+		}
+		if (!sedWorldList.isEmpty()) {
+			for (sedWorld ww: sedWorldList) {
+				if (org.bukkit.Bukkit.getWorld(worldName).getName().equals(ww.world.getName())) {
+					getLogger().info("Already enabled for this world");
+					return false;
+				}
+			}
+		}
+		/* nether/end world environments are not supported */
+		if (org.bukkit.Bukkit.getWorld(worldName).getEnvironment() != Environment.NORMAL) {
+			getLogger().info("Invalid environment for this world");
+			return false;
+		}
+
+		List<String> worldStringList = getConfig().getStringList("worlds");
+		if (worldStringList.indexOf(worldName) == -1) {
+			worldStringList.add(worldName);
+			getConfig().set("worlds", worldStringList);
+			saveConfig();
+		} else {
+			getLogger().info("Already configured enable for this world?!");
+		}
+
+		sedWorld s = new sedWorld();
+		s.world = org.bukkit.Bukkit.getWorld(worldName);
+		s.chunkMap = new HashMap<String, Integer>();
 
 		/* fill initial chunkmap here */
-		World world = org.bukkit.Bukkit.getWorld("world");
-		Chunk ChunkList[] = world.getLoadedChunks();
-		for (Chunk c: ChunkList) {
+		Chunk chunkList[] = s.world.getLoadedChunks();
+		for (Chunk c: chunkList) {
 			int x = c.getX();
 			int z = c.getZ();
-			ChunkMapLoad(x, z);
+			ChunkMapLoad(s.chunkMap, x, z);
 		}
+
+		sedWorldList.add(s);
+
+		return true;
+	}
+
+	public boolean disableWorld(String worldName)
+	{
+		if (org.bukkit.Bukkit.getWorld(worldName) == null) {
+			getLogger().info("No such world");
+			return false;
+		}
+		for (sedWorld ww: sedWorldList) {
+			if (org.bukkit.Bukkit.getWorld(worldName).getName().equals(ww.world.getName())) {
+				List<String> worldStringList = getConfig().getStringList("worlds");
+				worldStringList.remove(worldName);
+				getConfig().set("worlds", worldStringList);
+				saveConfig();
+
+				sedWorldList.remove(ww);
+
+				return true;
+			}
+		}
+
+		getLogger().info("world not currently enabled");
+
+		return false;
+	}
+
+	private int walker_start;
+	private int walker_step;
+	private int walker_phase;
+	private offset walker[];
+
+	public offset walker_f(offset o) {
+		/*
+		 * attempt to walk a grid as follows:
+		 * a(4)-b(4)-c(4)-d(8)-e(4)
+		 *    edcde
+		 *    dbabd
+		 *    caOac
+		 *    dbabd
+		 *    edcde
+		 */
+
+		if (o.x == 0 && o.z == 0)
+			walker_step = 0;
+
+		switch (walker_step) {
+			case 0:
+			case 4:
+			case 8:
+			case 20:
+				walker_start = (int)Math.round(rnd.nextDouble() * 4.0);
+				walker_phase = 4;
+				break;
+			case 12:
+				walker_start = (int)Math.round(rnd.nextDouble() * 8.0);
+				walker_phase = 8;
+				break;
+			default:
+				break;
+		}
+
+		int section_start = walker_step - (walker_step % walker_phase);
+		int section_part = ((walker_step - section_start) + walker_start) % walker_phase;
+
+		walker_step++;
+
+		return walker[section_start + section_part];
+	}
+
+	public void onEnable() {
+		sedWorldList = new ArrayList<sedWorld>();
+
+		/* these are used to displace blocks */
+		walker = new offset[24];
+
+		walker[0] = new offset(0,1);
+		walker[1] = new offset(1,0);
+		walker[2] = new offset(0,-1);
+		walker[3] = new offset(-1,0);
+
+		walker[4] = new offset(1,1);
+		walker[5] = new offset(1,-1);
+		walker[6] = new offset(-1,1);
+		walker[7] = new offset(-1,-1);
+
+		walker[8] = new offset(2,0);
+		walker[9] = new offset(-2,0);
+		walker[10] = new offset(0,2);
+		walker[11] = new offset(0,-2);
+
+		walker[12] = new offset(2,1);
+		walker[13] = new offset(2,-1);
+		walker[14] = new offset(1,2);
+		walker[15] = new offset(1,-2);
+		walker[16] = new offset(-1,2);
+		walker[17] = new offset(-1,-2);
+		walker[18] = new offset(-2,1);
+		walker[19] = new offset(-2,-1);
+
+		walker[20] = new offset(2,2);
+		walker[21] = new offset(-2,2);
+		walker[22] = new offset(2,-2);
+		walker[23] = new offset(-2,-2);
+
+		/* config data handling */
+		saveDefaultConfig();
+
+		List<String> worldStringList = getConfig().getStringList("worlds");
+
+		/* populate chunk cache for each world */
+		for (int i = 0; i < worldStringList.size(); i++)
+			enableWorld(worldStringList.get(i));
 
 		/* even handler takes care of updating it from there */
 		getServer().getPluginManager().registerEvents(new SedimentologyListener(), this);
 
-		/* these are used to displace blocks */
-		walker = new offset[8];
-		walker[0] = new offset(-1,-1);
-		walker[1] = new offset(0,-1);
-		walker[2] = new offset(1,-1);
-		walker[3] = new offset(1,0);
-		walker[4] = new offset(1,1);
-		walker[5] = new offset(0,1);
-		walker[6] = new offset(-1,1);
-		walker[7] = new offset(-1,0);
+		getCommand("sedimentology").setExecutor(new SedimentologyCommand());
 
-		walker2 = new offset[16];
-		walker2[0] = new offset(-2,-2);
-		walker2[1] = new offset(-1,-2);
-		walker2[2] = new offset(0,-2);
-		walker2[3] = new offset(1,-2);
-		walker2[4] = new offset(2,-2);
-		walker2[5] = new offset(2,-1);
-		walker2[6] = new offset(2,0);
-		walker2[7] = new offset(2,1);
-		walker2[8] = new offset(2,2);
-		walker2[9] = new offset(1,2);
-		walker2[10] = new offset(0,2);
-		walker2[11] = new offset(-1,2);
-		walker2[12] = new offset(-2,2);
-		walker2[13] = new offset(-2,1);
-		walker2[14] = new offset(-2,0);
-		walker2[15] = new offset(-2,-1);
-
+		BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
 		scheduler.scheduleSyncRepeatingTask(this, new SedimentologyRunnable(), 1L, 1L);
 	}
 }
-
-
-
 
